59cca1a16a556ba2d9f32c38c6516945fe028a95
lawlessowl <erokhin-maksim@bk.ru>
2024-10-06 23:55:42 +0300

    feat: solved 02/07 task

---
+++ lesson-02/07.js
@@ -0,0 +1,15 @@
+let passportMarried = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "Bobryisk"
+    }
+};
+
+let clonePassportMarried = structuredClone(passportMarried)
+
+clonePassportMarried.married = true
+
+console.log(passportMarried)
+console.log(clonePassportMarried)

dd173793871907fb23db42f20976ceb5b74cabf8
lawlessowl <erokhin-maksim@bk.ru>
2024-10-06 21:35:54 +0300

    feat: solved 02/05 task

---
+++ lesson-02/05.js
@@ -0,0 +1,12 @@
+let passport = {
+    name: "Petr",
+    surname: "Petrov",
+};
+
+let copyPassport = {}
+
+for (let key in passport) {
+    copyPassport[key] = passport[key]
+}
+
+copyPassport.name = "Ivan"

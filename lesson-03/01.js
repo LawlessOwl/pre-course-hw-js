464b05e6ff8c63eafc57b9c8f65155acf095d2b8
lawlessowl <erokhin-maksim@bk.ru>
2024-10-11 20:25:27 +0300

    feat: solved 03/01 task

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,8 @@
-// code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString() {
+    const germanColors = colors.filter(color => color === "черный" || color === "красный" || color === "желтый");
+    return germanColors.join('-');
+}
+
+console.log(createColorString());
